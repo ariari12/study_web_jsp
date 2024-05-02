0c696c5191f610928e8ccc80139dc21eb539ee36
ariari12 <kahback99@gmail.com>
2024-05-02 11:21:22 +0900

    aws jndi설정과 mysql 쿼리

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/web/awsTest.jsp
@@ -0,0 +1,38 @@
+<%@page import="vo.BoardVO"%>
+<%@page import="java.util.ArrayList"%>
+<%@page import="dao.BoardMySQLDAO"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<table>
+	<tr>
+		<th>게시물번호</th>
+		<th>제목</th>
+		<th>작성자</th>
+		<th>조회수</th>
+	</tr>
+	<%
+		BoardMySQLDAO dao
+			= new BoardMySQLDAO();
+	
+	ArrayList<BoardVO> list = dao.selectAll(0,20);
+	for(BoardVO vo : list){
+	%>
+	<tr>
+		<th><%= vo.getBno() %></th>
+		<th><%= vo.getTitle() %></th>
+		<th><%= vo.getWriter() %></th>
+		<th><%= vo.getHits() %></th>
+	</tr>
+	<%
+	}
+	%>
+	</table>
+</body>
+</html>
---
+++ web/src/main/java/dao/BoardMySQLDAO.java
@@ -0,0 +1,72 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import vo.BoardVO;
+
+public class BoardMySQLDAO {
+	
+	String driver = "com.mysql.cj.jdbc.Driver";
+	String url = "jdbc:mysql://xe.cr4eaioy6nur.ap-northeast-2.rds.amazonaws.com:3306/xe";
+	String user="scott";
+	String password="tigerlion2401$";
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+	StringBuffer sb = new StringBuffer();
+	
+	public BoardMySQLDAO() {
+		conn = DBConnection.getInstance().getConnection();
+		
+//		try {
+//			Class.forName(driver);
+//			conn = DriverManager.getConnection(url,user,password);
+//			System.out.println("conn : "+ conn);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
+	
+	public ArrayList<BoardVO> selectAll(int startNo, int recordPerPage){
+		sb.setLength(0);
+		sb.append("SELECT bno, writer, title, contents, regdate, hits, ip, status ");
+		sb.append("FROM BOARD ");
+		sb.append("ORDER BY bno desc ");
+		sb.append("limit ?,? ");
+		ArrayList<BoardVO> list = new ArrayList<BoardVO>();
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, startNo);
+			pstmt.setInt(2, recordPerPage);
+			
+			rs = pstmt.executeQuery();			
+			while(rs.next()) {
+				int bno = rs.getInt("bno");
+				String writer = rs.getString("writer");
+				String title = rs.getString("title");
+				String contents = rs.getString("contents");
+				String regdate = rs.getString("regdate");
+				int hits = rs.getInt("hits");
+				String ip = rs.getString("ip");
+				int status = rs.getInt("status");
+				BoardVO vo = new BoardVO(bno, writer, title, contents, regdate, hits, ip, status);
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+}
