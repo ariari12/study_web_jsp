04b17a9cb8ac2b9a6932a47c64a8fad0375cb47a
ariari12 <kahback99@gmail.com>
2024-05-03 11:02:46 +0900

    [feat] DB에서 데이터 읽기

--- myBatisEx1/src/test/TestMain.java
+++ myBatisEx1/src/test/TestMain.java
@@ -2,11 +2,14 @@ package test;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import vo.DeptVO;
 
 public class TestMain {
 	public static void main(String[] args) {
@@ -25,6 +28,26 @@ public class TestMain {
 			SqlSession ss=factory.openSession(true); // true : auto commit, false 수동커밋
 			
 			System.out.println("ss : "+ss);
+			
+			// JDBC	: Connection
+			// MyBatis : SqlSession
+			
+			// ss.selectList("별칭명");
+			
+			List<DeptVO> list =ss.selectList("AllDept");
+			
+			System.out.println("list : "+ list);
+			
+			// for문 사용해서 부서번호 : 부서명 : 위치 형식으로 출력
+			for(DeptVO vo : list) {
+				System.out.println("부서번호 : "+vo.getDeptno()+" 부서명 : "+vo.getDname()
+				+" 위치 : "+vo.getLoc());				 
+			}
+			System.out.println("===========================================================");
+			
+			// Stream 을 사용해서 부서번호 : 부서명 : 위치 형식으로 출력
+			list.stream().forEach(s->System.out.println("부서번호 : "+s.getDeptno()+" 부서명 : "+s.getDname()
+			+" 위치 : "+s.getLoc()));
 			
 		// Connection conn <== SqlSession <== SqlSessionFactory <== SqlSessionFactoryBuilder	
 		} catch (IOException e) {
