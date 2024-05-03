1ceffe20e1a68e62b3d7ba35adc78f9e68164528
ariari12 <kahback99@gmail.com>
2024-05-03 11:33:28 +0900

    [feat] mybatis CRUD

--- myBatisEx1/src/test/TestMain.java
+++ myBatisEx1/src/test/TestMain.java
@@ -29,8 +29,17 @@ public class TestMain {
 			
 			System.out.println("ss : "+ss);
 			
+			// Connection conn <== SqlSession <== SqlSessionFactory <== SqlSessionFactoryBuilder			
 			// JDBC	: Connection
 			// MyBatis : SqlSession
+			
+			
+			System.out.println("===========================================================");
+			// 1건 추가
+			DeptVO vo2 = new DeptVO(99,"회계","LA");
+			ss.insert("addOne",vo2);
+			
+			System.out.println("===========================================================");
 			
 			// ss.selectList("별칭명");
 			
@@ -49,7 +58,29 @@ public class TestMain {
 			list.stream().forEach(s->System.out.println("부서번호 : "+s.getDeptno()+" 부서명 : "+s.getDname()
 			+" 위치 : "+s.getLoc()));
 			
-		// Connection conn <== SqlSession <== SqlSessionFactory <== SqlSessionFactoryBuilder	
+			// 1건 데이터 읽기
+			DeptVO vo=ss.selectOne("SelectOne",20);
+			System.out.println("vo : "+vo);
+			
+			System.out.println("===========================================================");
+			
+			// 2번 부서 확인.. 1건 수정 부서명 IT, 지역 울산
+			DeptVO vo3 = new DeptVO();
+			
+			vo3.setDeptno(1);
+			vo3.setDname("IT");
+			vo3.setLoc("울산");
+			System.out.println(vo3);
+			ss.update("modifyOne",vo3);
+			
+			DeptVO vo4 = ss.selectOne("SelectOne",1);
+			System.out.println("수정 후 : "+vo4);
+			
+			System.out.println("===========================================================");
+			
+			//3번 부서를 삭제
+			ss.delete("deleteDeptOne",3);
+	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
